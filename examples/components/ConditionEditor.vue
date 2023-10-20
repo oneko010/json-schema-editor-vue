08d1073d77a367d8346d5acbc0e1e50737dc1d7f
cuihaicheng <cuihaicheng@aggrx.com>
2023-10-20 17:48:16 +0800

    feature:编辑必选属性

--- examples/components/ConditionEditor.vue
+++ examples/components/ConditionEditor.vue
@@ -1,8 +1,9 @@
 <script>
 import Vue from 'vue'
-import { Modal } from 'ant-design-vue'
+import { Modal, Checkbox } from 'ant-design-vue'
 Modal.install(Vue)
 export default {
+    components: { ACheckbox: Checkbox },
     data() {
         return {
             
@@ -58,6 +59,21 @@ export default {
 
                 return conditionNode
             }
+        },
+        propertyList: {
+            get: function() {
+                const node = this.pickValue
+                node.properties || this.$set(node, 'properties', {})
+                return Object.keys(node.properties)
+            }
+        },
+        selectedPropertyList() {
+            const node = this.pickValue
+            node.allOf || this.$set(node, 'allOf', [])
+
+            let index = this.index < 0 ? node.allOf.length - 1 : this.index
+            const thennode = node.allOf[index].then
+            return thennode.required
         }
     },
     methods: {
@@ -73,17 +89,56 @@ export default {
             if (node.allOf.length == 0) {
                 this.$delete(node,'allOf')
             }
-        }
+        },
+        propertySelectChanged(property) {
+            const node = this.pickValue
+            node.allOf || this.$set(node, 'allOf', [])
+
+            let index = this.index < 0 ? node.allOf.length - 1 : this.index
+            const thennode = node.allOf[index].then
+            if (thennode.required.includes(property)) {
+                const index = thennode.required.findIndex(item => item === property)
+                thennode.required.splice(index, 1)
+            } else {
+                thennode.required.push(property)
+            }
+        },
+        selectedAllChanged() {
+            const node = this.pickValue
+            node.allOf || this.$set(node, 'allOf', [])
+
+            let index = this.index < 0 ? node.allOf.length - 1 : this.index
+            const thennode = node.allOf[index].then
+            const selectedAll = this.selectedPropertyList.length != this.propertyList.length
+            if (selectedAll) {
+                this.propertyList.forEach(element => {
+                    if (!thennode.required.includes(element)) {
+                        thennode.required.push(element)
+                    }
+                })
+            } else {
+                thennode.required.splice(0, thennode.required.length)
+            }
+        },
+        
     }
 }
 </script>
 
 <template>
     <div>
-        <p>{{ condition }}</p>
+        <p>{{ propertyList }}</p>
         <json-schema-editor class="schema" :value="condition" disabledType lang="zh_CN" custom/>
+        <div class="required">
+        <a-checkbox v-for="item in propertyList" :key="item" :checked="selectedPropertyList.includes(item)" class="ant-col-name-required" @change="propertySelectChanged(item)">{{ item }}</a-checkbox>
+        <a-checkbox @change="selectedAllChanged" :checked="selectedPropertyList.length == propertyList.length">全选</a-checkbox>
+        </div>
     </div>
 </template>
 
 <style>
+.required {
+    margin-left: 20px;
+    margin-top: 10px;
+}
 </style>
